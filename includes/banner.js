3e39a702039015499bad1eea9c528655ef136469
alex <moose@Alexs-MacBook-Pro-2.local>
2018-06-23 17:58:43 -0400

    updated css styling

---
+++ includes/banner.js
@@ -0,0 +1,10 @@
+document.write('\
+<div class="container banner">\
+    <h1>Alex Bock</h1>\
+    <p>\
+        <b>student</b> | \
+        <a href="mailto:alexander.bock@tufts.edu":>email</a> | \
+        <a href="https://www.linkedin.com/in/alex-bock-09a753121/">linkedin</a>\
+    </p>\
+</div>\
+');
---
+++ includes/navbar.js
@@ -0,0 +1,9 @@
+document.write('\
+<div class="navbar">\
+    <li><a href="index.html">home</a></li>\
+    <li><a href="bio.html">bio</a></li>\
+    <li><a href="#">academics</a></li>\
+    <li><a href="#">research</a></li>\
+    <li><a href="Resume.pdf" download="abock.pdf">resume</a></li>\
+</div>\
+');
