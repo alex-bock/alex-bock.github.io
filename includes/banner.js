43f29c9f2cef10b9cdf131eece90cfa0d2389927
alex <moose@Alexs-MacBook-Pro-2.local>
2018-06-23 20:36:19 -0400

    more styling

--- includes/banner.js
+++ includes/banner.js
@@ -4,7 +4,8 @@ document.write('\
     <p>\
         <b>student</b> | \
         <a href="mailto:alexander.bock@tufts.edu":>email</a> | \
-        <a href="https://www.linkedin.com/in/alex-bock-09a753121/">linkedin</a>\
+        <a href="https://github.com/alex-bock" target="_blank">github</a> | \
+        <a href="https://www.linkedin.com/in/alex-bock-09a753121/" target="_blank">linkedin</a>\
     </p>\
 </div>\
 ');
--- includes/navbar.js
+++ includes/navbar.js
@@ -1,9 +1,9 @@
 document.write('\
 <div class="navbar">\
     <li><a href="index.html">home</a></li>\
-    <li><a href="bio.html">bio</a></li>\
+    <li><a href="research.html">research</a></li>\
     <li><a href="#">academics</a></li>\
-    <li><a href="#">research</a></li>\
     <li><a href="Resume.pdf" download="abock.pdf">resume</a></li>\
+    <li><a href="bio.html">bio</a></li>\
 </div>\
 ');
