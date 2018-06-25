7470b87f1ec32059d5e62e5bb20bfc40a7a8b8c4
alex <moose@Alexs-MacBook-Pro-2.local>
2018-06-25 12:01:49 -0400

    new resume and new layouts

--- includes/navbar.js
+++ includes/navbar.js
@@ -3,7 +3,7 @@ document.write('\
     <li><a href="index.html">home</a></li>\
     <li><a href="research.html">research</a></li>\
     <li><a href="#">academics</a></li>\
-    <li><a href="Resume.pdf" download="abock.pdf">resume</a></li>\
+    <li><a href="Resume.pdf" target="_blank">resume</a></li>\
     <li><a href="bio.html">bio</a></li>\
 </div>\
 ');
