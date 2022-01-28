72b19285b10d77ccb807aa1110f828a611ef6678
alex-bock <alex.bock125@gmail.com>
2022-01-28 16:24:07 -0500

    initial commit

---
+++ includes/banner.js
@@ -0,0 +1,11 @@
+document.write('\
+<div class="container banner">\
+    <h1>Alex Bock</h1>\
+    <p>\
+        <b>student</b> | \
+        <a href="mailto:alexander.bock@tufts.edu":>email</a> | \
+        <a href="https://github.com/alex-bock" target="_blank">github</a> | \
+        <a href="https://www.linkedin.com/in/alex-bock-09a753121/" target="_blank">linkedin</a>\
+    </p>\
+</div>\
+');
